5e90ffc3d83a89d29bf94bc011c485377dbcbcfa
tarun genwa <tarungenwa@taruns-MacBook-Pro.local>
2019-01-29 22:55:01 +0530

    apollourl

--- src/apolloclient/index.js
+++ src/apolloclient/index.js
@@ -1,12 +1,12 @@
-import ApolloClient from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import ApolloClient from 'apollo-client';
 import { ApolloLink } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
 import { withClientState } from 'apollo-link-state';
-import { HttpLink } from 'apollo-link-http'
 
 let uri = 'http://localhost:8000/graphql'
 if(process.env.NODE_ENV === 'production'){
-    uri = 'https://sattamatkabazaar.com/api/graphql'
+    uri = 'https://biddingapiserver.herokuapp.com/graphql'
 }
 const httpLink = new HttpLink({ uri })
 
